0addfb87078c570967eb743504380cc5bfd02aa0
rafsanopi <rafsan3@dti.ac>
2025-05-11 16:36:13 -0600

    Docker connected

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello!!");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcome");
+});
+
+app.listen(8000, () => {
+  console.log("Server started at port 8000");
+});
