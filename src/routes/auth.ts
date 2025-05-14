e40f2674c92d872ba8616292b6791c2291f0e2e9
rafsanopi <rafsan3@dti.ac>
2025-05-13 18:50:25 -0600

    User Schema Done

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,10 +1,25 @@
-import { Router } from "express";
+import { Router, Request } from "express";
 
 const authRouter = Router();
+
+interface SignUpBody {
+  name: string;
+  email: string;
+  password: string;
+}
+
+authRouter.post("/signup", async (req: Request<{}, {}, SignUpBody>, res) => {
+  try {
+    // Get request body 
+    // Check User already exist or not
+    // Create new user and Store in db
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+});
 
 authRouter.get("/", (req, res) => {
   res.send("Login Page");
 });
 
 export default authRouter;
- 
